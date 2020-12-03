8804f53858f6dcec7c18002b09ce1412e191369d
edccc <1742648313@qq.com>
2020-12-03 11:36:26 +0800

    项目初始化，和下载第三方包

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+// 挂载axios到原型
+Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  render: function (h) { return h(App) }
+  render: function (h) {
+    return h(App)
+  }
 }).$mount('#app')
