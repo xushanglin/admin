eb1ef4cac010a432ab34ee57b2e466fd1a480cf3
edccc <1742648313@qq.com>
2020-12-04 16:09:25 +0800

    分页小组讲和优化翻页功能

--- src/views/Postlist.vue
+++ src/views/Postlist.vue
@@ -1,34 +1,44 @@
 <template>
-  <el-table :data="postList" style="width: 100%">
-    <el-table-column label="日期" width="180">
-      <template>
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">2020-12-03</span>
-      </template>
-    </el-table-column>
-    <el-table-column label="缩略图" width="200">
-      <template slot-scope="scope">
-        <img
-          v-if="scope.row.cover.length > 0"
-          :src="scope.row.cover[0].url | fixImgUrl"
-          class="thumbnail"
-        />
-        <img v-else src="@/assets/logo.png" class="thumbnail" />
-      </template>
-    </el-table-column>
-    <el-table-column label="标题" prop="title"> </el-table-column>
-    <el-table-column
-      label="作者"
-      width="140"
-      prop="user.nickname"
-    ></el-table-column>
-    <el-table-column label="操作">
-      <template>
-        <el-button size="mini">编辑</el-button>
-        <el-button size="mini" type="danger">删除</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-table :data="postList" style="width: 100%">
+      <el-table-column label="日期" width="180">
+        <template>
+          <i class="el-icon-time"></i>
+          <span style="margin-left: 10px">2020-12-03</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="缩略图" width="200">
+        <template slot-scope="scope">
+          <img
+            v-if="scope.row.cover.length > 0"
+            :src="scope.row.cover[0].url | fixImgUrl"
+            class="thumbnail"
+          />
+          <img v-else src="@/assets/logo.png" class="thumbnail" />
+        </template>
+      </el-table-column>
+      <el-table-column label="标题" prop="title"> </el-table-column>
+      <el-table-column
+        label="作者"
+        width="140"
+        prop="user.nickname"
+      ></el-table-column>
+      <el-table-column label="操作">
+        <template>
+          <el-button type="warning" size="small">编辑</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @current-change="currentChange"
+      @size-change="sizeChange"
+      layout="prev,pager,next,sizes, jumper"
+      :total="total"
+      :page-size="pageSize"
+      :page-sizes="[2, 5, 10]"
+    >
+    </el-pagination>
+  </div>
 </template>
 
 
@@ -37,16 +47,37 @@ export default {
   data() {
     return {
       postList: [],
+      pageIndex: 1,
+      pageSize: 5,
+      total: 0,
     };
   },
-  methods: {},
+  methods: {
+    loadPage() {
+      this.$axios({
+        url: "/post",
+        params: {
+          pageSize: this.pageSize,
+          pageIndex: this.pageIndex,
+        },
+      }).then((res) => {
+        console.log(res.data);
+        this.postList = res.data.data;
+        this.total = res.data.total;
+      });
+    },
+    currentChange(newValue) {
+      // console.log(newValue);
+      this.pageIndex = newValue;
+      this.loadPage();
+    },
+    sizeChange(newValue) {
+      this.pageSize = newValue;
+      this.loadPage();
+    },
+  },
   created() {
-    this.$axios({
-      url: "/post",
-    }).then((res) => {
-      console.log(res.data.data);
-      this.postList = res.data.data;
-    });
+    this.loadPage();
   },
 };
 </script>
